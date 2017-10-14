0391889f0684700da377d2803b4ca633dc3680f3
Frank Müller <frank.mueller@softart.ch>
2017-10-14 10:47:22 +0200

    Adds timer to the 3D viewer GUI allowing fixed timer interval updates of the position
    Relates:
    https://github.com/FrankMueller/BobOnRails/issues/6

--- Source/BobOnRails.Controller.GUI/MainViewModel.cs
+++ Source/BobOnRails.Controller.GUI/MainViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows.Media;
+﻿using System;
+using System.Windows.Media;
 using System.Windows.Media.Media3D;
+using System.Windows.Threading;
 using HelixToolkit.Wpf;
 
 namespace BobOnRails.Controller.GUI
@@ -9,40 +11,55 @@ namespace BobOnRails.Controller.GUI
     /// </summary>
     public class MainViewModel
     {
+        private GeometryModel3D bob;
+
         /// <summary>
         /// Gets or sets the model.
         /// </summary>
-        /// <value>The model.</value>
         public Model3D Model { get; set; }
+
+        public Point3DCollection Path { get; set; }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="MainViewModel"/> class.
         /// </summary>
         public MainViewModel()
         {
-            // Create a model group
-            var modelGroup = new Model3DGroup();
-
-            // Create a mesh builder and add a box to it
-            var meshBuilder = new MeshBuilder(false, false);
-            meshBuilder.AddBox(new Point3D(0, 0, 0.25), 1, 2, 0.5);
-            meshBuilder.AddCylinder(new Point3D(0.5, 0.5, 0.5), new Point3D(0.5, 0.5, 1.5), 0.25, 18);
-
-            // Create a mesh from the builder (and freeze it)
-            var mesh = meshBuilder.ToMesh(true);
-
-            // Create some materials
+            //Create some materials
             var greenMaterial = MaterialHelper.CreateMaterial(Colors.Green);
             var blueMaterial = MaterialHelper.CreateMaterial(Colors.Blue);
 
-            // Add 3 models to the group (using the same mesh, that's why we had to freeze it)
-            modelGroup.Children.Add(new GeometryModel3D { Geometry = mesh, Material = greenMaterial });
+            //Create a mesh geometry representing Bob
+            var meshBuilder = new MeshBuilder(false, false);
+            meshBuilder.AddBox(new Point3D(0, 0, 0.25), 1, 2, 0.5);
+            meshBuilder.AddCylinder(new Point3D(0.5, 0.5, 0.5), new Point3D(0.5, 0.5, 1.5), 0.25, 18);
+            bob = new GeometryModel3D { Geometry = meshBuilder.ToMesh(true), Material = greenMaterial };
+
+            //Create a geometry representing the path Bob travelled
+            Path = new Point3DCollection();
 
             var grid = new GridLinesVisual3D() { Width = 10, Length = 10, MinorDistance = 1, MajorDistance = 5, Thickness = 0.01, Material = blueMaterial };
+            
+            //Create a model group
+            var modelGroup = new Model3DGroup();
+            modelGroup.Children.Add(bob);
             modelGroup.Children.Add(grid.Model);
 
-            // Set the property, which will be bound to the Content property of the ModelVisual3D (see MainWindow.xaml)
             Model = modelGroup;
+
+            var timer = new DispatcherTimer();
+            timer.Tick += OnTimerTick;
+            timer.Interval = TimeSpan.FromMilliseconds(20);
+            timer.Start();
+        }
+
+        private void OnTimerTick(object sender, EventArgs e)
+        {
+            var random = new Random();
+            var point = new Point3D(random.NextDouble(), random.NextDouble(), random.NextDouble());
+            Path.Add(point);
+
+            bob.Transform = new TranslateTransform3D(point.X, point.Y, point.Z);
         }
     }
 }
